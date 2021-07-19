ce729b3a9687b114db47641efde9d77e64ba203b
Mostafa-702 <87659938+Mostafa-702@users.noreply.github.com>
2021-07-19 19:30:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
 client.user.setActivity("Made in Github");
-consile.log("Ready");
-})
+console.log(bot.user.username + " Ready");
+});
 
 client.on('message', async message => {
 if(message.author.bot) return;
 if(!message.guild) return;
 if(message.content.startsWith('ping')) {
-message.channel.send(Date.now() - message.createdTimestamp)
+message.channel.send(Date.now() - message.createdTimestamp);
 }
 });
 
