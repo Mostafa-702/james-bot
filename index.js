547cc6285e2b25d481fb487357f2956877ff0124
Mostafa-702 <87659938+Mostafa-702@users.noreply.github.com>
2021-07-19 18:21:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ if(!message.guild) return;
 if(message.content.startsWith('ping')) {
 message.channel.send(Date.now() - message.createdTimestamp)
 }
-})
+});
 
-client.login("");
+client.login("ODY2MzY5NDI1Mjc5NDE4Mzg5.YPRjZw.n1yYJ5_dIW2IB0Tih07-zOivj8s");
